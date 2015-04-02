028fe2130fcbe7526f7d9162b353f7646bf8e71c
VISTALL <vistall.valeriy@gmail.com>
2015-04-02 18:20:42 +0300

    compilation

--- src/org/gark87/intellij/lang/ini/psi/IniAstLeafFactory.java
+++ src/org/gark87/intellij/lang/ini/psi/IniAstLeafFactory.java
@@ -4,6 +4,7 @@ import org.gark87.intellij.lang.ini.parsing.IniTokenTypes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import com.intellij.lang.ASTLeafFactory;
+import com.intellij.lang.LanguageVersion;
 import com.intellij.psi.impl.source.tree.LeafElement;
 import com.intellij.psi.tree.IElementType;
 
@@ -15,7 +16,7 @@ public class IniAstLeafFactory implements ASTLeafFactory
 {
 	@NotNull
 	@Override
-	public LeafElement createLeaf(IElementType type, CharSequence text)
+	public LeafElement createLeaf(@NotNull IElementType type, @NotNull LanguageVersion<?> languageVersion, @NotNull CharSequence text)
 	{
 		return new SectionImpl(type, text);
 	}
