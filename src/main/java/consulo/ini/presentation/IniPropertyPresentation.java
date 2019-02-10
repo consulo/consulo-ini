35ee23010db6fa652d557a1aac50a869d90379f7
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-02-10 09:29:38 +0300

    compilation

--- src/main/java/consulo/ini/presentation/IniPropertyPresentation.java
+++ src/main/java/consulo/ini/presentation/IniPropertyPresentation.java
@@ -1,15 +1,14 @@
 package consulo.ini.presentation;
 
 import javax.annotation.Nullable;
-import javax.swing.Icon;
 
 import org.gark87.intellij.lang.ini.psi.IniProperty;
 import org.gark87.intellij.lang.ini.psi.IniSection;
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.ItemPresentationProvider;
 import consulo.annotations.RequiredReadAction;
-import consulo.awt.TargetAWT;
 import consulo.ide.IconDescriptorUpdaters;
+import consulo.ui.image.Image;
 
 /**
  * @author VISTALL
@@ -52,8 +51,8 @@ public class IniPropertyPresentation implements ItemPresentation
 
 	@Nullable
 	@Override
-	public Icon getIcon(boolean unused)
+	public Image getIcon()
 	{
-		return TargetAWT.to(IconDescriptorUpdaters.getIcon(myItem, 0));
+		return IconDescriptorUpdaters.getIcon(myItem, 0);
 	}
 }
